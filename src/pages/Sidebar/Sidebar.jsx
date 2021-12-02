3434372604cef54c0e6477270badfe1208990252
BigodeSan <claudomiromonteiro@gmail.com>
2021-12-02 13:16:38 -0300

    add icons

--- src/pages/Sidebar/Sidebar.jsx
+++ src/pages/Sidebar/Sidebar.jsx
@@ -4,6 +4,11 @@ import { DropdownFeminino } from '../../components/DropdownFemi/DropdownFeminino
 import { DropdownMasculino } from '../../components/DropdownMasc/DropdownMasculino'
 import { Avatar, Background, Li, Link, SidebarNav, Ul } from './style'
 
+import { AiFillHome, AiFillMail } from 'react-icons/ai'
+import { FaFemale, FaMale } from 'react-icons/fa'
+import { RiUserSharedFill } from 'react-icons/ri'
+import { GiPositionMarker } from 'react-icons/gi'
+
 export const Sidebar = () => {
 
     const scrollToTop = () => {
@@ -33,66 +38,72 @@ export const Sidebar = () => {
                     <Ul>
                         <Li>
                             <Link activeClass="active"
-                            to="/"
-                            spy={true}
-                            smooth={true}
-                            hashSpy={true}
-                            offset={-60}
-                            duration={400}
-                            delay={100}
-                            onClick={scrollToTop}
+                                to="/"
+                                spy={true}
+                                smooth={true}
+                                hashSpy={true}
+                                offset={-60}
+                                duration={400}
+                                delay={100}
+                                onClick={scrollToTop}
                             >
+                                <AiFillHome />
                                 Inicio
                             </Link>
                         </Li>
                         <Li>
                             <Link to="/" onClick={handleClickFemi}>
+                                <FaFemale />
                                 Pijama Feminino
                             </Link>
                             {dropdowmFemi ? (<DropdownFeminino />) : null}
                         </Li>
                         <Li>
                             <Link to="/" onClick={handleClickMasc}>
+                                <FaMale />
                                 Pijama Masculino
                             </Link>
                             {dropdowmMasc ? (<DropdownMasculino />) : null}
                         </Li>
                         <Li>
                             <Link activeClass="active"
-                            to="quemsomos"
-                            spy={true}
-                            smooth={true}
-                            hashSpy={true}
-                            offset={-60}
-                            duration={400}
-                            delay={100}
+                                to="quemsomos"
+                                spy={true}
+                                smooth={true}
+                                hashSpy={true}
+                                offset={-60}
+                                duration={400}
+                                delay={100}
                             >
+                                <RiUserSharedFill />
                                 Quem Somos
                             </Link>
                         </Li>
                         <Li>
                             <Link activeClass="active"
-                            to="localizacao"
-                            spy={true}
-                            smooth={true}
-                            hashSpy={true}
-                            offset={-60}
-                            duration={400}
-                            delay={100}
+                                to="localizacao"
+                                spy={true}
+                                smooth={true}
+                                hashSpy={true}
+                                offset={-60}
+                                duration={400}
+                                delay={100}
                             >
+                                <GiPositionMarker />
                                 Localização
                             </Link>
                         </Li>
                         <Li>
                             <Link activeClass="active"
-                            to="contato"
-                            spy={true}
-                            smooth={true}
-                            hashSpy={true}
-                            offset={-60}
-                            duration={400}
-                            delay={100}
+                                to="contato"
+                                spy={true}
+                                smooth={true}
+                                hashSpy={true}
+                                offset={-60}
+                                duration={400}
+                                delay={100}
                             >
+                                <AiFillMail />
                                 Contato
                             </Link>
                         </Li>
