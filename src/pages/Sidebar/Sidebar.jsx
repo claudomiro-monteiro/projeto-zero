2961143cdd3154b689c922c5fd878304f0e5d1b1
BigodeSan <claudomiromonteiro@gmail.com>
2021-12-01 21:24:33 -0300

    teste again2

--- src/pages/Sidebar/Sidebar.jsx
+++ src/pages/Sidebar/Sidebar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Avatar, Background, Li, Link, SidebarNav, Ul } from './style'
 
 export const Sidebar = () => {
     return (
-        <div>
-            Sidebar
-        </div>
+        <>
+            <Background>
+                <SidebarNav>
+                    <Avatar>
+                        Imagem
+                    </Avatar>
+                    <Ul>
+                        <Li>
+                            <Link to="/">
+                                Pijama Feminino
+                            </Link>
+                        </Li>
+                    </Ul>
+                </SidebarNav>
+            </Background>
+        </>
     )
 }
