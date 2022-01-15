47cb0dc513607d40ae00675cda65a3f135a6f0e2
BigodeSan <claudomiromonteiro@gmail.com>
2022-01-14 22:18:55 -0300

    teste

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-scroll'
-import { FaPhone } from 'react-icons/fa'
 
 import { Adress, Footers, RedeSocial } from './style'
 
@@ -51,7 +50,7 @@ export const Footer = () => {
                 <div className="link">
                     <h3>Contato</h3>
                     <a href="tel:1231576205" className="social">
-                        <FaPhone size="3rem" />
+                        {/* <FaPhone size="3rem" /> */}
                         <div className="hide">(12) 3157-6205</div>
                     </a>
                     <a href="https://wa.me/5512988383481" className="social">
