bbe113a59a26d97806fd760304e518ea066e1be6
BigodeSan <claudomiromonteiro@gmail.com>
2022-01-17 22:17:21 -0300

    add icones

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-scroll';
-import { MdOutlineFacebook } from "react-icons/md";
+// import { MdOutlineFacebook } from "react-icons/md";
 import { Adress, Footers, LinkRede, RedeSocial } from './style';
 
 export const Footer = () => {
@@ -69,7 +69,7 @@ export const Footer = () => {
                             {/* <FaFacebookF size="3rem" /> */}
                         </LinkRede>
                         <LinkRede href="https://www.instagram.com/sensual.moda.intima/">
-                            <MdOutlineFacebook />
+                            {/* <MdOutlineFacebook /> */}
                         </LinkRede>
                     </div>
                 </RedeSocial>
