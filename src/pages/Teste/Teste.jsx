b5a50d2d27360db4fa1e0f50efd74d63d2140696
BigodeSan <claudomiromonteiro@gmail.com>
2022-01-18 12:50:11 -0300

    teste FaInstagram

--- src/pages/Teste/Teste.jsx
+++ src/pages/Teste/Teste.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FaInstagram } from 'react-icons/fa'
 
 export const Teste = () => {
     return (
         <div>
             Ola isto é um teste
+            <FaInstagram />
         </div>
     )
 }
