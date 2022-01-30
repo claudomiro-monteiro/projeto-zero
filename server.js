753e5a38ae2b68ad12f8b011c7b27767594aca48
BigodeSan <claudomiromonteiro@gmail.com>
2022-01-30 20:27:33 -0300

    remove express.json

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(require('cors')());
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 const upload = require('multer')();
