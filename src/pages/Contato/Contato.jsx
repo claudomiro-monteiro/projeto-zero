080375cdf59dd05c075c828a9dbb7f07a9e50b45
claudomiro-monteiro <claudomiromonteiro@gmail.com>
2022-04-20 23:02:47 -0300

    modifiquei botão enviar

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -173,7 +173,7 @@ export const Contato = () => {
                         <Button>
                             <button type="submit">
                                 {spinner && <FontAwesomeIcon icon={faSpinner} spin />}
-                                {!spinner && <span>Enviar <IoMdSend /></span>}
+                                {!spinner && <span>Enviar Mensagem<IoMdSend /></span>}
                             </button>
                         </Button>
                     </ButtonMsg>
